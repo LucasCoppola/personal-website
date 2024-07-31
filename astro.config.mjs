d191ad66e44853de26cb90139dcd1a018896ecbd
LucasCoppola <lucascoppola21@gmail.com>
2024-07-31 11:31:18 -0300

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  output: "server",
+  site: "https://lucascoppola.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
